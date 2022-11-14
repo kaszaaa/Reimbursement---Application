b05ba3f2e9070f556e7b117e98f154549b978d26
unknown <kkaszka97@wp.pl>
2022-11-14 16:13:12 +0100

    repaired program

---
+++ src/main/java/com/revature/HelloSpringMvc1Application.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringMvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringMvc1Application.class, args);
+	}
+
+}
